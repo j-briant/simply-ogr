0de4fcf8747f45cf426f85bd0a41dfde016a2e58
j-briant <j.briant@protonmail.com>
2023-03-30 16:49:51 +0200

    su to postgres during script

--- ogr_transfer.sh
+++ ogr_transfer.sh
@@ -3,10 +3,10 @@
 # TRANSFERT DATA 
 
 jq -c '.[]' /etc/ogr_transfer/conf.json | while read i; do
-	ogr2ogr PG:dbname=gc_transfert $(jq -r '.source_connexion' <<< "$i") \
-		-sql @/etc/ogr_transfer/$(jq -r '.sql' <<< "$i") \
-		-oo LIST_ALL_TABLES=YES -oo SCHEMAS=$(jq -r '.source_schema' <<< "$i") \
-		-lco SCHEMA=$(jq -r '.destination_schema' <<< "$i") -lco OVERWRITE=YES \
-		--config PG_USE_COPY YES --config OGR_TRUNCATE YES \
-		-nln $(jq -r '.destination_table' <<< "$i")
+su - postgres -c "ogr2ogr PG:dbname=gc_transfert $(jq -r '.source_connexion' <<< "$i") \
+			-sql @/etc/ogr_transfer/sql/$(jq -r '.sql' <<< "$i") \
+			-oo LIST_ALL_TABLES=YES -oo SCHEMAS=$(jq -r '.source_schema' <<< "$i") \
+			-lco SCHEMA=$(jq -r '.destination_schema' <<< "$i") -lco OVERWRITE=YES \
+			--config PG_USE_COPY YES --config OGR_TRUNCATE YES \
+			-nln $(jq -r '.destination_table' <<< "$i")"
 done
