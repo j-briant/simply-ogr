0f55aa306797677fa4f887cb275d1a2a0eb8c96f
sgca0260 <julien.briant@lausanne.ch>
2023-04-26 17:11:35 +0200

    add the destination_connexion parameter

--- ogr_transfer.sh
+++ ogr_transfer.sh
@@ -3,7 +3,7 @@
 # TRANSFERT DATA 
 
 jq -c '.[]' /etc/ogr_transfer/conf.json | while read i; do
-su - postgres -c "ogr2ogr PG:dbname=gc_transfert $(jq -r '.source_connexion' <<< "$i") \
+su - postgres -c "ogr2ogr $(jq -r '.destination_connexion' <<< "$i") $(jq -r '.source_connexion' <<< "$i") \
 			-sql @/etc/ogr_transfer/sql/$(jq -r '.sql' <<< "$i") \
 			-oo LIST_ALL_TABLES=YES -oo SCHEMAS=$(jq -r '.source_schema' <<< "$i") \
 			-lco SCHEMA=$(jq -r '.destination_schema' <<< "$i") -lco OVERWRITE=YES \
